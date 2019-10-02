348f26d5849febceaa59472f71113e3ce3c18c12
Python_Phu <Python_Phu@DESKTOP-D7MBTIA>
2019-10-02 15:32:26 +0700

    Commit EP SSH

---
+++ src/main/java/scripts/EPCommand.java
@@ -0,0 +1,29 @@
+package scripts;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+import frame.EPCommonFunction;
+import lib.ReadData;
+import lib.SSHCommand;
+
+//import static lib.ReadData.*;
+public class EPCommand {
+    
+	protected Logger log = Logger.getLogger(this.getClass().getName());
+	ReadData readObj = new ReadData();
+	EPCommonFunction epcoObj = new EPCommonFunction();
+	
+@Test
+   public void checkCommand() {
+	
+	SSHCommand sshObj = new SSHCommand();
+	sshObj.connect(readObj.EPServer, readObj.EPUser_SSH, readObj.EPPass_SSH);
+	String cmd = "service vpms status";
+	String cmd1 = "iaversion.php";
+	//log.info(cmd1);
+	sshObj.exec(cmd1);
+	sshObj.disconnect();
+	epcoObj.wait(5);
+}
+}
