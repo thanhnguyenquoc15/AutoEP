375a485daa9d63b8f44551e8f56291e74994abca
Thanh Nguyen Quoc <nqthanh1@tma.com.vn>
2019-10-02 15:14:41 +0700

    update

--- src/main/java/frame/AuditLogViewerPage.java
+++ src/main/java/frame/AuditLogViewerPage.java
@@ -1,23 +1,41 @@
 package frame;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 import lib.BrowserFactory;
 import lib.ReadData;
 
 public class AuditLogViewerPage {
 
+	WebDriver driver = BrowserFactory.getDriver(ReadData.BROWSER);
 
 	protected Logger log = Logger.getLogger(this.getClass().getName());
-	WebDriver driver = BrowserFactory.getDriver(ReadData.BROWSER);
+	protected EPCommonFunction ComFunc = PageFactory.initElements(driver, EPCommonFunction.class);
+	
+	private static String zonesBox = "auditLogViewerZone";
+	private static String categoriesBox = "logOptionsCatNames";
+	private static String actionsBox = "logOptionsActionNames";
+
+	
+	private WebElement searchKeys = driver.findElement(By.xpath("//input[@id=\"auditLogViewerForm:auditkeywords\"]"));
+	
+	private static String sortBy = "logOptionsSortBy";
+	
+	public WebElement elementsBox(String elementsBox) {
+		WebElement checkBox = driver.findElement(By.xpath("select[@id=\"auditLogViewerForm:" + elementsBox + "']"));
+		return checkBox;
+	}
 	
 	
 	//generate Audit Log Report using multiple parameters in the Audit Log Viewer Page 
 	public void generateAuditLogs(	String zones, 
 									String categories, 
 									String actions,
-									String searchKeywords,
+									String keywords,
 									String sortBy,
 									String predefinedValues,
 									String lastXDays,
@@ -25,8 +43,14 @@ public class AuditLogViewerPage {
 									String startDate,
 									String endDate)
 		{
+		ComFunc.selectMultiElementsBox(elementsBox(zonesBox), zones);
+		ComFunc.selectMultiElementsBox(elementsBox(categoriesBox), categories);
+		ComFunc.selectMultiElementsBox(elementsBox(actionsBox), actions);
+		searchKeys.sendKeys(keywords);
+		elementsBox(sortBy).sendKeys("");
 		
-	}
+		
+		}
 	
 	
 }
--- src/main/java/frame/EPCommonFunction.java
+++ src/main/java/frame/EPCommonFunction.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import lib.BrowserFactory;
 import lib.ReadData;
@@ -16,10 +18,14 @@ public class EPCommonFunction {
 	
 	private String mainFrame = "main";
 	
+	private String headerFrame = "header";
+	
+	WebDriver driver = BrowserFactory.getDriver(ReadData.BROWSER);
 	
 	protected Logger log = Logger.getLogger(this.getClass().getName());
 	
-	WebDriver driver = BrowserFactory.getDriver(ReadData.BROWSER);
+	
+	
 	
 	// select the page to handle from EPM Menu Bar	
 		public void switchEPMenu(String page) {
@@ -45,6 +51,26 @@ public class EPCommonFunction {
 			driver.switchTo().frame(frame);
 			
 		}
-	
-	//writing method 2567
+		
+	/* 
+	 * @author: tqnguyen@avaya.com
+	 * @last modified date:
+	 * @desc opens a modal window to display a message
+	 * @param box: the selected box
+	 * @param attribute: the attribute that would be selected in the box
+	 * @return bool - success or failure
+	 */
+		public  void selectMultiElementsBox(WebElement box,String attribute) {
+			try
+			{
+				Select select = new Select(box);
+				select.selectByValue(attribute);
+			}
+			catch(Exception e)
+			{
+				log.info("fail to select "+ attribute);
+			}
+			
+		}
+
 }
