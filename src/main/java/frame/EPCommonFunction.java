64d7e1d61193d7143a70dbb4975638a8499a3c22
Python_Phu <Python_Phu@DESKTOP-D7MBTIA>
2019-10-02 12:40:13 +0700

    Commit select drop down

--- src/main/java/frame/EPCommonFunction.java
+++ src/main/java/frame/EPCommonFunction.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 import lib.BrowserFactory;
 import lib.ReadData;
@@ -45,6 +45,71 @@ public class EPCommonFunction {
 			driver.switchTo().frame(frame);
 			
 		}
+		
+		/**
+		 * This method is used to wait execution or process till user defined time.
+		 *
+		 * @param    secs time specified in seconds
+		 * @return   Noting .
+		 *
+		 */
+		public void wait(int secs)
+		{
+			// call a native sleep
+			try
+			{
+				log.debug("Entering into Method : " + Thread.currentThread().getStackTrace()[1].getMethodName());
+				Thread.sleep(secs * 1000);
+			} catch (Exception ex)
+			{
+				log.error("Exception occurred in {createDIR} method...");
+			}
+		}
+		
+		/**
+		 This method is used to get selected dropdown option.
+		 *
+		 * @param   driver Selenium WebDriver object
+		 * @param   element Dropdown/Combo box object
+		 * @return   String - returns selected value from dropdown .
+		 *
+		 */
+		public String getDropDownOption(WebDriver driver, WebElement element)
+		{
+			log.debug("Entering into Method : " + Thread.currentThread().getStackTrace()[1].getMethodName());
+			// GetText from the drop down list
+			String selectedOption = new Select(element).getFirstSelectedOption().getText();
+			// return selection option
+			return (selectedOption);
+		}
+
+
+		/**
+		 This method is used to select value from dropdown list.
+		 *
+		 * @param   driver Selenium WebDriver object
+		 * @param   element Dropdown/Combo box object
+		 * @param   sItem Item to select from dropdown
+		 *
+		 * @return  String - returns selected value from dropdown .
+		 *
+		 */
+		public boolean selectDropDownOption(WebDriver driver, WebElement element, String sItem)
+		{
+			log.debug("Entering into Method : " + Thread.currentThread().getStackTrace()[1].getMethodName());
+			try
+			{
+				Select selectedOption = new Select(element);
+				//selectedOption.selectByValue(sItem);
+				selectedOption.selectByVisibleText(sItem);
+				return true;
+			} catch (Exception e)
+			{
+				//new ExceptionHandler(e,Thread.currentThread().getStackTrace()[1].getMethodName());
+				return false;
+			}
+		}
+
 	
-	//writing method 2567
+	//writing method
 }
