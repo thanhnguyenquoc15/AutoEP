9b7f5541b5ff8bbb04098d87e46449ec26545589
Thanh Nguyen Quoc <nqthanh1@tma.com.vn>
2019-10-10 15:06:33 +0700

    add SSH page

---
+++ src/main/java/lib/SSHCommands.java
@@ -0,0 +1,188 @@
+package lib;
+
+import java.io.*;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+import com.google.common.io.CharStreams;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class SSHCommands {
+
+	protected Logger log = Logger.getLogger(this.getClass().getName());
+
+//	/**
+//	 * Connect to remote machine and establish session.
+//	 * written by POM team
+//	 * @return if connect successfully, return true, else return false
+//	 */
+//	public Boolean connect(String sHostName, String sUserName, String sPassword) {
+//		try {
+//			session = jsch.getSession(sUserName, sHostName);
+//			log.info("Session initialized with Host [" + sHostName + "] and associated with user [" + sUserName
+//					+ "] and credential " + sPassword);
+//			// session.setUserInfo(userInfo);
+//			session.setPassword(sPassword);
+//			config.put("StrictHostKeyChecking", "no");
+//			session.setConfig(config);
+//			log.info("SSHExec initialized successfully");
+//			log.info("SSHExec trying to connect " + sUserName + "@" + sHostName);
+//			session.connect();
+//			log.info("SSH connection established");
+//			return true;
+//		} catch (Exception e) {
+//			log.info("Connect fails with the following exception: " + e);
+//			return false;
+//		}
+//	}
+
+	/**
+	 * Connect to remote machine and send 1 command only then exit.
+	 *
+	 * @return 
+	 */
+	public void sshConnect(	
+//							String user, 
+//							String password, 
+//							String host, 
+							String command) throws JSchException, InterruptedException, IOException {
+		java.util.Properties config = new java.util.Properties();
+		config.put("StrictHostKeyChecking", "no");
+		JSch jsch = new JSch();
+
+		Session session = jsch.getSession(ReadData.OSUSER, ReadData.EPServer, 22);
+
+		log.info("SSHExec trying to connect " + ReadData.OSUSER + "@" + ReadData.EPServer);
+		//hardcode password temp
+		session.setPassword(ReadData.OSPASS);
+		session.setConfig(config);
+		session.connect();
+		log.info("SSH connection established");
+		Channel channel = session.openChannel("exec");
+		((ChannelExec) channel).setErrStream(System.err);
+		log.info("Connection channel established successfully");
+		((ChannelExec) channel).setCommand(command);
+		channel.setInputStream(null);
+		log.info("Start to run command: " + command);
+		channel.connect();
+		InputStream in = channel.getInputStream();
+		String result = CharStreams.toString(new InputStreamReader(in));
+		log.info(result);
+		channel.disconnect();
+		session.disconnect();
+	}
+//	retain session solutions	
+//	private Session getSession() throws Exception {
+//	    try {
+//	        ChannelExec testChannel = (ChannelExec) session.openChannel("exec");
+//	        testChannel.setCommand("true");
+//	        testChannel.connect();
+//
+//	            log.debug("Session successfully tested, use it again.");
+//
+//	        testChannel.exit();
+//	    } catch (Throwable t) {
+//	        log.info("Session terminated. Create a new one.");
+//	        session = jsch.getSession(user, host, port);
+//	        session.setConfig(config);
+//	        session.connect();
+//	    }
+//	    return session;
+//	}
+	/**
+	 * Connect to remote machine and send multiple commands then exit.
+	 * @input multiple commands to send to server
+	 * @return console log
+	 */	
+	public void sshConnectShell(String... commands) throws Exception {
+		java.util.Properties config = new java.util.Properties();
+		config.put("StrictHostKeyChecking", "no");
+		JSch jsch = new JSch();
+		Session session = jsch.getSession(ReadData.OSUSER, ReadData.EPServer, 22);
+		log.info("SSHExec trying to connect " + ReadData.OSUSER + "@" + ReadData.EPServer);
+		session.setPassword(ReadData.OSPASS);
+		session.setConfig(config);
+		session.connect();
+		Thread.sleep(2000);
+		log.info("SSH connection established");
+		Channel channel = session.openChannel("shell");
+
+		OutputStream out = channel.getOutputStream();
+		PrintStream ps = new PrintStream(out, true);
+		// open channel
+		channel.connect();
+		InputStream input = channel.getInputStream();
+
+		for (String command : commands) {
+			ps.print(command + "\n");
+			log.info("Start to run command: " + command);
+		}
+		ps.close();
+
+		// print Result
+		printResult(input, channel);
+
+//		String result = CharStreams.toString(new InputStreamReader(input));
+//		log.info(result);
+		channel.disconnect();
+		session.disconnect();
+	}
+
+	public int iExitStatus;
+	/**
+	 * @param input
+	 * @param channel
+	 */
+	private void printResult(InputStream input, Channel channel) throws Exception {
+
+//		InputStream in = channel.getInputStream();
+//		String result = CharStreams.toString(new InputStreamReader(input));
+//		log.info(result);
+		
+		Date startDate = new Date();
+		int timeout = 10;
+		StringBuilder sb = new StringBuilder();
+		int SIZE = 1024;
+		byte[] tmp = new byte[SIZE];
+		while (true) {
+			while (input.available() > 0) {
+				Date endDate = new Date();
+				long difference = (endDate.getTime() - startDate.getTime()) / 1000;
+				int i = input.read(tmp, 0, SIZE);
+				if (i < 0)
+					break;
+				String str = (new String(tmp, 0, i));
+				sb.append(str);
+				log.info(str);
+				if (difference >= timeout) {
+					log.info("timed out. command took more than expected");
+					break;
+				}
+			}
+			if (channel.isClosed()) {
+				log.info("Connection channel closed");
+				log.info("Check if exec success or not ... ");
+				iExitStatus = channel.getExitStatus();
+				if (iExitStatus == 0) {
+					log.info("Execute successfully for command");
+				} else {
+					log.info("Execution failed while executing command");
+					log.info("Error message: " + ((ChannelExec) channel).getErrStream());
+				}
+				break;
+			}
+			Date endDate = new Date();
+			long difference = (endDate.getTime() - startDate.getTime()) / 1000;
+			if (difference >= timeout) {
+				log.info("timed out. command took more than expected");
+				break;
+			}
+		}
+	}
+}
--- src/main/java/scripts/emailUI.java
+++ src/main/java/scripts/emailUI.java
@@ -7,6 +7,9 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.Session;
+
 import bsh.ParseException;
 import frame.AuditLogViewerPage;
 import frame.EPCommonFunction;
@@ -15,6 +18,7 @@ import frame.EPMServersPage;
 import frame.RolesPage;
 import frame.UsersPage;
 import lib.ReadData;
+import lib.SSHCommands;
 
 public class emailUI extends HelperClass{
 
@@ -24,6 +28,7 @@ public class emailUI extends HelperClass{
 	protected UsersPage userPageObj;
 	protected EPMServersPage EPMServersObj;
 	protected AuditLogViewerPage auditLogsObj;
+	protected SSHCommands ssh;
 	
 	@Override
 	public  void performBeforeMethodOperation(){
@@ -33,15 +38,22 @@ public class emailUI extends HelperClass{
 	userPageObj = PageFactory.initElements(driver, UsersPage.class);
 	EPMServersObj = PageFactory.initElements(driver, EPMServersPage.class);
 	auditLogsObj = PageFactory.initElements(driver, AuditLogViewerPage.class);
+	ssh = PageFactory.initElements(driver, SSHCommands.class);
 	
 	Logger log = Logger.getLogger(this.getClass().getName());
 	}
 	
 	@Override
     public void performAfterMethodOperation(){
-
     }
 	
+	
+	
+	/* 
+	 * @author: tqnguyen@avaya.com
+	 * @last modified date:
+	 * @desc all step follow test case email Addresses on QC 
+	 */
 	@Test
 	(dataProvider = "dataMap", dataProviderClass = ReadData.class)
 	public void emailAddresses(Hashtable testData) throws Exception,
@@ -57,11 +69,11 @@ public class emailUI extends HelperClass{
 		
 		
 		//add multiple valid email addresses to each group and select Save.
-		EPMServersObj.configAlarmDelivery(1, "1st Group", "nqthanh1@tma.com.vn;");
-		EPMServersObj.configAlarmDelivery(2, "2nd Group", "nqthanh1@tma.com.vn;");
+		EPMServersObj.configAlarmDelivery("1", "1st Group", "nqthanh1@tma.com.vn;","","");
+		EPMServersObj.configAlarmDelivery("2", "2nd Group", "nqthanh1@tma.com.vn;nqthanh3@tma.com.vn;nqthanh2@tma.com.vn;","","");
 		
-		//+ Verify the email address list for each group has been updated.
-		EPMServersObj.button("Apply").click();
+		//Verify the email address list for each group has been updated.
+		
 		//Verify 
 		
 		
@@ -70,14 +82,38 @@ public class emailUI extends HelperClass{
 		auditLogsObj.generateAuditLogs("Default", "Alarm", "Add", "", "Time: oldest first", "", "1", "Hours", "", "");
 		
 		//Access EPM Servers > Alarm Codes > Change Alarm Delivery Settings,
+		ComFuncObj.switchEPMenu("EPM Servers");
+		EPMServersObj.button("Alarm Codes").click();
+		EPMServersObj.button("Delivery Settings").click();
+		EPMServersObj.configAlarmDelivery("2", "2st Group Edited", "nqthanh2@tma.com.vn","","");
 		//remove at least one email address from each group and select Save.
+		EPMServersObj.button("Apply").click();
 		//Access Audit Log Viewer. + Verify an audit log entry exists for the changes.
+		auditLogsObj.generateAuditLogs("Default", "Alarm", "Add", "", "Time: oldest first", "", "1", "Hours", "", "");
 		//Access EPM Servers > Alarm Codes > Change Alarm Delivery Settings, 
 		//add a non-email address to each group and select Save. + Verify the non-email address entry is not retained.
-
+		EPMServersObj.configAlarmDelivery("2", "2st Group Edited", "nqthanh2aaaaaaa","true","");
 		
 		
 	}
-	
-	
+
+	/*
+	 * @author: tqnguyen@avaya.com
+	 * 
+	 * @last modified date:
+	 * 
+	 * @desc
+	 */
+	@Test(dataProvider = "dataMap", dataProviderClass = ReadData.class)
+	public void SSHTesting(Hashtable testData) throws Exception, ParseException, IOException {
+		
+//		ssh.sshConnect("service appserver status && service vpms status");
+//		ssh.sshConnect("service appserver status&&service vpms status");
+//		ssh.sshConnectShell("cd /tmp&&lenhTaoCert");
+		ssh.sshConnectShell("service vpms status","cd /tmp","pwd");
+//		ssh.exeCmd(channel, "cd /tmp");
+//		ssh.exeCmd(channel, "pwd");
+
+	}
+
 }
